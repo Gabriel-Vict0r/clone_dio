e4037c0350312430148c70fa22daa86e6ca32d41
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2022-12-21 17:24:43 -0300

    first commit for DIO clone

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import { Button } from "../../Components/button";
+import { Header } from "../../Components/header";
+const Home = () => {
+    return (<>
+        <Header></Header>
+
+        </>)
+}
+
+export {Home};
